207a2064ba217dd6b339c33117c89bb3a3d85e7f
Archer Morgan <armorgan@microsoft.com>
2021-10-14 09:34:15 -0700

    updates

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -1,32 +1,14 @@
 export const MenuItems = [
     {
-        title: 'About',
+        title: 'Info',
         url: '#',
         cName: 'nav-links',
         icon: 'fas fa-question'
     },
     {
-        title: 'My Data',
+        title: 'Memes',
         url: '#',
         cName: 'nav-links',
-        icon: 'fas fa-star-half-alt'
+        icon: 'fas fa-question'
     },
-    {
-        title: 'Current Problem Set',
-        url: '#',
-        cName: 'nav-links',
-        icon: 'fas fa-pencil-alt'
-    },
-    {
-        title: 'Editor',
-        url: '#',
-        cName: 'nav-links',
-        icon: 'fas fa-book-open'
-    },
-    {
-        title: 'New Book',
-        url: '#',
-        cName: 'nav-links',
-        icon: 'far fa-plus-square'
-    }
 ]
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,29 +3,9 @@ import { MenuItems } from "./MenuItems";
 import { Button } from "./Button";
 import './Navbar.css';
 import { Link } from 'react-router-dom';
-import { useIsAuthenticated, MsalProvider, useMsal, useAccount } from "@azure/msal-react";
 
 
 function Navbar() {
-
-    const isAuthenticated = useIsAuthenticated();
-    const { instance } = useMsal();
-    const [clicked, setClicked] = useState(false);
-
-    const signOut = () => {
-        instance.logout();
-    }
-
-    const signIn = async () => {
-        try {
-            const loginResponse = await instance.loginPopup({});
-        } catch (err) {
-            console.log(err);
-        }
-        window.location.reload();
-    }
-
-    const loginButton = isAuthenticated? <Button onClick={signOut} >Sign Out</Button> : <Button onClick={signIn} >Sign In</Button>
 
     const navStyleSignedIn = {
             color: "black",
@@ -34,26 +14,15 @@ function Navbar() {
             padding: "0.5rem 1rem"
     };
 
-    const navStyleSignedOut = {
-        color: "grey",
-        fontFamily: "Palatino",
-        textDecoration: 'none',
-        padding: "0.5rem 1rem"
-};
-
     return (
         <nav className="NavbarItems">
 
-            <h1 className="navbar-logo">Selfteach</h1>
+            <h1 className="navbar-logo">🤩DOUBLE FEATURE🤩</h1>
 
-            <div className="menu-icon" onClick={ () => setClicked( !clicked )}>
-                <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-            </div>
-
-            <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
+            <ul className={'nav-menu'}>
                 {MenuItems.map((item, index) => {
                     return (
-                        <Link key={index} to={isAuthenticated? `/${clean(item.title)}` : "#"} style={isAuthenticated? navStyleSignedIn : navStyleSignedOut}>
+                        <Link key={index} to={`/${clean(item.title)}`} style={navStyleSignedIn}>
                             <li>
                                 {item.title}
                                 <i className={item.icon} ></i>
@@ -63,7 +32,6 @@ function Navbar() {
                 })}
                 <NavButton/>
             </ul>
-            {loginButton}
         </nav>
     );
 }
